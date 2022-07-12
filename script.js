0d1ee5af960101ae5b177bd6c2800eff29ddc84f
Admin <hemamane5376@gmail.com>
2022-07-12 17:13:10 +0530

    array

---
+++ script.js
@@ -0,0 +1,79 @@
+// Array 
+// let nums = [1,2,3,4]
+
+// let hemasfamily =  {
+//     name : 'hema',
+//     age : 21,
+//     profession : 'Software',
+//     brother : {
+//         name : 'Govind',
+//         age : 15,
+//         accupation:'student',
+//     }
+
+
+// }
+// console.log(hemasfamily.brother.accupation);
+
+
+// console.log(hemasfamily.brother.accupation);
+
+// console.log(hemasfamily['brother']['accupation']);
+
+// console.log(hemasfamily.brother.age);
+
+// // console.log(hemasfamily['brother']['age']);
+
+// console.log(hemasfamily['name']);
+
+// console.log(hemasfamily.age);
+// // console.log(nums[0]);
+
+
+// let nums = [1,2,3,4]
+// let num = {
+//     0 : 1,
+//     1 : 2,
+//     2 : 3,
+//     3 : 'hema',
+//     5:100,
+// }
+// console.log(num[3],num[10]);
+// let hemasfamily =  {
+//     name : 'hema',
+//     age : 21,
+//     profession : 'Software',
+//     brother : {
+//         name : 'Govind',
+//         age : 15,
+//         accupation:'student',
+//     }
+
+
+// }
+// console.log(hemasfamily);
+
+
+// let nums =  [7,5]
+// let copy = nums;
+
+// console.log(nums,copy);
+// copy[1] = 16;
+// nums[0] = 6;
+
+// console.log(nums,copy);
+
+
+// let nums =10 ;
+// let copy = 30;
+// nums = 80;
+// copy = 3;
+// console.log(nums,copy);
+
+
+
+
+
+
+
+
